01b08d433845fc6b0dcee690b6cf1f3d1fd9d08d
András Gábor Köles <andras.koles@stud.uni-corvinus.hu>
2021-11-26 17:25:55 +0100

    first steps

---
+++ Usermainentance/entities/user.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usermainentance.entities
+{
+    class user
+    {
+        public Guid guid { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+       
+        public string Fullname
+        {
+            get { return Lastname + " " + FirstName; }
+        }
+    }
+}
