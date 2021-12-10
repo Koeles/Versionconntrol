bb7d0fa8d471fd77e3be2854c42203a096ef08b5
András Gábor Köles <andras.koles@stud.uni-corvinus.hu>
2021-12-10 20:25:32 +0100

    ini

---
+++ lbxmml/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace lbxmml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chartRatedata = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tolpicker = new System.Windows.Forms.DateTimePicker();
+            this.igpicker = new System.Windows.Forms.DateTimePicker();
+            this.cbvaluta = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRatedata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(404, 263);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chartRatedata
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartRatedata.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartRatedata.Legends.Add(legend2);
+            this.chartRatedata.Location = new System.Drawing.Point(460, 38);
+            this.chartRatedata.Name = "chartRatedata";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartRatedata.Series.Add(series2);
+            this.chartRatedata.Size = new System.Drawing.Size(347, 322);
+            this.chartRatedata.TabIndex = 1;
+            this.chartRatedata.Text = "chart1";
+            // 
+            // tolpicker
+            // 
+            this.tolpicker.Location = new System.Drawing.Point(460, 12);
+            this.tolpicker.Name = "tolpicker";
+            this.tolpicker.Size = new System.Drawing.Size(112, 20);
+            this.tolpicker.TabIndex = 2;
+            this.tolpicker.ValueChanged += new System.EventHandler(this.tolpicker_ValueChanged);
+            // 
+            // igpicker
+            // 
+            this.igpicker.Location = new System.Drawing.Point(578, 12);
+            this.igpicker.Name = "igpicker";
+            this.igpicker.Size = new System.Drawing.Size(118, 20);
+            this.igpicker.TabIndex = 3;
+            this.igpicker.ValueChanged += new System.EventHandler(this.tolpicker_ValueChanged);
+            // 
+            // cbvaluta
+            // 
+            this.cbvaluta.FormattingEnabled = true;
+            this.cbvaluta.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.cbvaluta.Location = new System.Drawing.Point(702, 11);
+            this.cbvaluta.Name = "cbvaluta";
+            this.cbvaluta.Size = new System.Drawing.Size(95, 21);
+            this.cbvaluta.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbvaluta);
+            this.Controls.Add(this.igpicker);
+            this.Controls.Add(this.tolpicker);
+            this.Controls.Add(this.chartRatedata);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRatedata)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRatedata;
+        private System.Windows.Forms.DateTimePicker tolpicker;
+        private System.Windows.Forms.DateTimePicker igpicker;
+        private System.Windows.Forms.ComboBox cbvaluta;
+    }
+}
+
---
+++ lbxmml/Form1.cs
@@ -0,0 +1,106 @@
+﻿using lbxmml.Entitites;
+using lbxmml.MNBServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
+
+namespace lbxmml
+{
+    public partial class Form1 : Form
+    {
+
+        BindingList<Ratedata> _rates= new BindingList<Ratedata>();
+        BindingList<string> currencies = new BindingList<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbvaluta.DataSource = currencies;
+            Refreshdata();
+        }
+
+        private void Refreshdata()
+        {
+            if (cbvaluta.SelectedItem == null) return;
+                _rates.Clear();
+            loadXml(getRates());
+            dataGridView1.DataSource = _rates;
+
+            makeChart();
+        }
+
+        private void makeChart()
+        {
+            chartRatedata.DataSource = _rates;
+            Series sorozatok = chartRatedata.Series[0];
+            sorozatok.ChartType = SeriesChartType.Line;
+            sorozatok.XValueMember = "Date";
+            sorozatok.YValueMembers = "value";
+
+            var jelmagyarazat = chartRatedata.Legends[0];
+            jelmagyarazat.Enabled = false;
+            var diagramterulet = chartRatedata.ChartAreas[0];
+            diagramterulet.AxisY.IsStartedFromZero = false;
+            diagramterulet.AxisY.MajorGrid.Enabled = false;
+            diagramterulet.AxisX.MajorGrid.Enabled = false;
+
+
+        }
+
+        private void loadXml(string xmlstring)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                Ratedata r = new Ratedata();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                var childElement = (XmlElement)item.ChildNodes[0];
+                r.Currency = childElement.GetAttribute("curr");
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+                r.Value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+                _rates.Add(r);
+            }
+        }
+
+        private string getRates()
+        {
+            
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
+            req.currencyNames = cbvaluta.SelectedItem.ToString();//"EUR";
+            req.startDate = tolpicker.Value.ToString("yyyy-MM-dd");//2020-01-01";
+            req.endDate = igpicker.Value.ToString("yyyy-MM-dd");//"2020-06-30";
+            var response = mnbService.GetExchangeRates(req);
+            return response.GetExchangeRatesResult;
+        }
+        private string getCurrencies()
+        {
+
+            var mnbServvice = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody req = new GetCurrenciesRequestBody();
+            var resp = mnbServvice.GetCurrencies(req);
+            return resp.GetCurrenciesResult;
+
+        }
+
+
+        private void tolpicker_ValueChanged(object sender, EventArgs e)
+        {
+
+            Refreshdata();
+        }
+
+     
+    }
+}
